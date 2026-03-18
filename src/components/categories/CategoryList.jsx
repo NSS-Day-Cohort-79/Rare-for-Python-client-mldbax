300ed037adc3960bb43e8e5ef5aaaf853c351d60
Larissa Ferreira <larissadferr@gmail.com>
2026-03-18 15:43:35 -0500

    CategoryList - updated class names

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -12,17 +12,25 @@ export const CategoryList = () => {
 
   return (
     <>
-      <div class="content is-normal">
-        <h1>Categories</h1>
-        {categoryArray.map((category) => {
-          return (
-            <div class="tags has-addons are-medium" key={category.id}>
-              <a class="tag is-primary is-hoverable" href="/">{category.label}</a>
-              <a class="tag is-light" href="/">&#9881;</a>
-              <a class="tag is-delete" href="/"> </a>
-            </div>
-          );
-        })}
+      <div class="container">
+        <div class="section is-normal">
+          <h1 class="title">Categories</h1>
+          {categoryArray.map((category) => {
+            return (
+              <div class="tags has-addons are-medium" key={category.id}>
+                <a class="tag is-primary is-hoverable" href="/">
+                  {category.label}
+                </a>
+                <a class="tag is-light" href="/">
+                  &#9881;
+                </a>
+                <a class="tag is-delete" href="/">
+                  {" "}
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
