41696dc9d2bd11f1d812c1efb06d1b98800575bf
Larissa Ferreira <larissadferr@gmail.com>
2026-03-19 14:09:26 -0500

    NewCategoryForm - draft

--- src/components/categories/NewCategoryForm.jsx
+++ src/components/categories/NewCategoryForm.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "../../managers/CategoryManager";
+
 export const NewCategoryForm = () => {
-  return <></>;
+  const [userInput, setUserInput] = useState({
+    id: 0,
+    label: "",
+  });
+  const [allCategories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((categories) => setCategories(categories));
+  }, []);
+
+  return (
+    <>
+      <div class="container">
+        <div class="section is-normal">
+          <h1 class="title">Create New Category</h1>
+          <div class="field">
+            <label class="label">Category Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Category" />
+            </div>
+          </div>
+          <div class="field">
+            <button class="button is-link" label="Submit">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
