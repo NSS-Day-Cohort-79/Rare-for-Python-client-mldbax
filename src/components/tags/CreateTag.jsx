c6b956b95e86190392a30d72217bf38e91f0f1e4
cmdrumright <cdrumright@protonmail.com>
2026-03-19 15:51:05 -0500

    - Added input box and state to hold label

--- src/components/tags/CreateTag.jsx
+++ src/components/tags/CreateTag.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export const CreateTag = () => {
-  return <>Create Tag</>;
+  const [label, setLabel] = useState("");
+
+  return (
+    <>
+      Create Tag
+      <input
+        className="input"
+        type="text"
+        value={label}
+        onChange={(e) => setLabel(e.target.value)}
+      />
+    </>
+  );
 };
