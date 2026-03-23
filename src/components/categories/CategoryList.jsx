3ab8dbe34851936aafd2a10126cd3c13a97875cc
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-23 18:02:49 -0500

    linked create new category to new category page

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { getCategories } from "../../managers/CategoryManager";
-import { Link } from "react-router-dom";
+import { deleteCategory, getCategories } from "../../managers/CategoryManager";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export const CategoryList = () => {
   const [categoryArray, setCategoryArray] = useState([]);
+  const navigate = useNavigate()
+  const { id } = useParams()
 
   useEffect(() => {
     getCategories().then((categories) => {
@@ -11,24 +13,31 @@ export const CategoryList = () => {
     });
   }, []);
 
+  // handle delete passing category id add alert to make sure admin wants to delete it
+  const handleDelete = () => {
+    alert("Are you sure you want to delete this category?")
+    deleteCategory(id).then(() => {
+      navigate("/categories")
+     })
+   }
+
   return (
     <>
-      <div class="container">
-        <div class="section is-normal">
-          <h1 class="title">Categories</h1>
+      <div className="container">
+        <div className="section is-normal">
+          <h1 className="title">Categories</h1>
           {/* add link for Create New Category, route to /new */}
-          <Link>Create New Category</Link>
+          <Link to="new" className="has-text-primary">Create New Category</Link>
           {categoryArray.map((category) => {
             return (
-              <div class="tags has-addons are-medium" key={category.id}>
-                <a class="tag is-primary is-hoverable" href="/">
+              <div className="tags has-addons are-medium" key={category.id}>
+                <a className="tag is-primary is-hoverable" href="/">
                   {category.label}
                 </a>
-                <a class="tag is-light" href="/">
+                <a className="tag is-light" href="/">
                   &#9881;
                 </a>
-                <a class="tag is-delete" href="/">
-                  {" "}
+                <a className="tag is-delete" href="/" onClick={handleDelete}>
                 </a>
               </div>
             );
