16b58428f82fefed861caefea2129c52c525bf6d
Larissa Ferreira <larissadferr@gmail.com>
2026-03-24 10:53:21 -0500

    Comments - added post state for page title, updated react component to handle 0 comments on post, updated classes, added navigation buttons

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getCommentsByPostId } from "../../managers/CommentsManager";
+import { getPostById } from "../../managers/PostsManager";
 
 export const Comments = () => {
   const { postId } = useParams();
+  const navigate = useNavigate();
   const [comments, setComments] = useState([]);
+  const [post, setPost] = useState({});
 
   useEffect(() => {
     getCommentsByPostId(postId).then((c) => {
@@ -12,11 +15,55 @@ export const Comments = () => {
     });
   }, [postId]);
 
+  useEffect(() => {
+    getPostById(postId).then((p) => {
+      setPost(p);
+    });
+  }, [postId]);
+
   return (
     <>
       <div className="container">
         <div className="section is-normal">
-          <h1 className="title">{comments[0]?.post?.title}</h1>
+          <h1 className="title">"{post.title}" Comments</h1>
+          <div className="block level">
+            <button
+              className="button is-link level-left"
+              onClick={() => navigate(`/posts/${postId}`)}
+            >
+              Return to Post
+            </button>
+            <button
+              className="button is-primary level-right"
+              onClick={() => navigate(`new`)}
+            >
+              New Comment
+            </button>
+          </div>
+
+          <div className="fixed-grid has-2-cols">
+            <div className="grid">
+              {comments && comments.length > 0 ? (
+                comments.map((comment) => (
+                  <div className="cell box" key={comment.id}>
+                    <div className="title">{comment.subject}</div>
+                    <div className="content">{comment.content}</div>
+                    <div className="level">
+                      <div className="level-left">
+                        {comment.author.username}
+                      </div>
+                      <div className="level-right">{comment.created_on}</div>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className="level-item">
+                  No comments on this post yet, add a comment to start the
+                  conversation!
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
