c33da3f0f006fd02a28d3d6688bf01f67d285123
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-24 14:39:35 -0500

    Added styling to create tags page to match categories

--- src/components/tags/CreateTag.jsx
+++ src/components/tags/CreateTag.jsx
@@ -8,28 +8,44 @@ export const CreateTag = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (label.trim() === '') {
-      alert('Please input a tag')
-    }
-      else {
+    if (label.trim() === "") {
+      alert("Please input a tag");
+    } else {
       createTag({
         label: label,
-      }).then(() => { navigate(-1) });
+      }).then(() => {
+        navigate(-1);
+      });
     }
   };
 
   return (
     <>
-      Create Tag
-      <input
-        className="input"
-        type="text"
-        value={label}
-        onChange={(e) => setLabel(e.target.value)}
-      />
-      <button className="button" onClick={handleSubmit}>
-        create
-      </button>
+      <div className="container">
+        <div className="section is-normal">
+          <h1 className="title">Create New Tag</h1>
+          <div className="field">
+            <label className="label">Tag Name</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                value={label}
+                onChange={(e) => setLabel(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <button
+              className="button is-link"
+              label="Submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
