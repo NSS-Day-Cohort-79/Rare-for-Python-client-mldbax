3d9d5c922e964f27850a6c7a533dee3ad325cc80
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-24 14:38:54 -0500

    formatted on save

--- src/components/categories/NewCategoryForm.jsx
+++ src/components/categories/NewCategoryForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export const NewCategoryForm = () => {
   const [label, setLabel] = useState("");
   const [allCategories, setCategories] = useState([]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // get list of current categories
   useEffect(() => {
@@ -25,18 +25,16 @@ export const NewCategoryForm = () => {
   // handleSaveCategory, call handle category check function, if false, api call, if true, return error
   const handleSaveCategory = (e) => {
     e.preventDefault();
-    if (label.trim() === '') {
-      alert('Please input a category')
-    }
-    else if (handleCategoryCheck(label)) {
-      alert('Category already exists')
-    }
-    else {
+    if (label.trim() === "") {
+      alert("Please input a category");
+    } else if (handleCategoryCheck(label)) {
+      alert("Category already exists");
+    } else {
       createCategory({ label: label }).then(() => {
-        navigate(-1)
-       })
-     }
-  }
+        navigate(-1);
+      });
+    }
+  };
 
   return (
     <>
@@ -46,14 +44,22 @@ export const NewCategoryForm = () => {
           <div className="field">
             <label className="label">Category Name</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Category"
+              <input
+                className="input"
+                type="text"
+                placeholder="Category"
                 value={label}
-                onChange={(e) => setLabel(e.target.value) } />
+                onChange={(e) => setLabel(e.target.value)}
+              />
             </div>
           </div>
           <div className="field">
             {/* create onClick save category in button */}
-            <button className="button is-link" label="Submit" onClick={handleSaveCategory}>
+            <button
+              className="button is-link"
+              label="Submit"
+              onClick={handleSaveCategory}
+            >
               Submit
             </button>
           </div>
