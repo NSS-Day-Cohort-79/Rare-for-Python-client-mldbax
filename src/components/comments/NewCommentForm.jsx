25d4b3025c9a915a413654c51af1fc2e11a3f3cc
Larissa Ferreira <larissadferr@gmail.com>
2026-03-25 10:26:07 -0500

    NewCommentForm - built html

--- src/components/comments/NewCommentForm.jsx
+++ src/components/comments/NewCommentForm.jsx
@@ -11,5 +11,61 @@ export const NewCommentForm = ({ token }) => {
     content: "",
   });
 
-  return <></>;
+  const handleChange = (e) => {
+    const copy = { ...comment };
+    const id = e.target.id;
+    copy[id] = e.target.value;
+    setComment(copy);
+  };
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <div className="container">
+        <div className="section is-normal">
+          <h1 className="title">New Comment</h1>
+          <div className="form">
+            <div className="block field">
+              <label className="label">Subject</label>
+              <input
+                className="input"
+                type="text"
+                id="subject"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="block field">
+              <label className="label">Comment</label>
+              <input
+                className="input"
+                type="text"
+                id="content"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <button
+                className="button is-link"
+                label="Submit"
+                // onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+            <div className="field">
+              <button
+                className="button is-warning"
+                label="Cancel"
+                // onClick={navigate(-1)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
