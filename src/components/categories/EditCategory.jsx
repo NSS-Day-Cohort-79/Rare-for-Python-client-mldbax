e6b0681976d6451f27e8329845a80c1fd05e7e9d
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-25 16:10:29 -0500

    finished feature and fixed auto-fill bug

--- src/components/categories/EditCategory.jsx
+++ src/components/categories/EditCategory.jsx
@@ -1,16 +1,69 @@
 import { useEffect, useState } from "react";
+import { getCategoryById, updateCategory } from "../../managers/CategoryManager";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const EditCategory = () => {
-  const [getCategory, setCategory] = useState({
-    label: "",
-  });
+  const [editedCategory, setEditedCategory] = useState({}) 
+  const { id } = useParams()
+  const navigate = useNavigate()
 
+ //fetch category data and populate editedCategory
   useEffect(() => {
-    getCategory.then(setCategory)
-  }, [getCategory])
+    getCategoryById(id).then((catObj) => {
+      console.log(catObj)
+      setEditedCategory(catObj)
+     })
+  }, [id])
   
+  //handle input change
+  const handleChange = (event) => {
+    const { name, value, type } = event.target;
+    const copy = { ...editedCategory };
+    if (type === "text") {
+      copy[name] = value
+    }
+    setEditedCategory(copy)
+   }
+
+  //handle updated category and navigate back to category list
+  const handleUpdateCategory = () => {
+    updateCategory(id, editedCategory).then(() => {
+      navigate(`/categories`)
+     })
+   } 
 
   return (
-    <>edit post</>
+    <>
+      <div className="container">
+        <div className="section is-normal">
+          <h1 className="title">Edit Category</h1>
+          <div className="field">
+            <label className="label">Category Name</label>
+            <div className="control">
+              <input
+                name="label"
+                type="text"
+                value={editedCategory?.label || ""}
+                onChange={handleChange}
+                required
+                className="input"
+              />
+            </div>
+          </div>
+          <div className="field">
+            {/* create onClick save category in button */}
+            <button
+              className="button is-link"
+              label="Submit"
+              onClick={() => {
+                handleUpdateCategory();
+               }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
