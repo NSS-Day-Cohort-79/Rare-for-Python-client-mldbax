1d703b5dc9da16db8d813f73ae2677212e06fad2
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-26 10:53:50 -0500

    created Edit Tag component

---
+++ src/components/tags/EditTag.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { getCategoryById, updateCategory } from "../../managers/CategoryManager";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const EditCategory = () => {
+  const [editedTag, setEditedTag] = useState({}) 
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+ //fetch category data and populate editedCategory
+  useEffect(() => {
+    getTagById(id).then((tagObj) => {
+      setEditedTag(tagObj)
+     })
+  }, [id])
+  
+  //handle input change
+  const handleChange = (event) => {
+    const { name, value, type } = event.target;
+    const copy = { ...editedCategory };
+    if (type === "text") {
+      copy[name] = value
+    }
+    setEditedCategory(copy)
+   }
+
+  //handle updated category and navigate back to category list
+  const handleUpdateCategory = () => {
+    updateCategory(id, editedCategory).then(() => {
+      navigate(`/categories`)
+     })
+   } 
+
+  return (
+    <>
+      <div className="container">
+        <div className="section is-normal">
+          <h1 className="title">Edit Category</h1>
+          <div className="field">
+            <label className="label">Category Name</label>
+            <div className="control">
+              <input
+                name="label"
+                type="text"
+                value={editedCategory?.label || ""}
+                onChange={handleChange}
+                required
+                className="input"
+              />
+            </div>
+          </div>
+          <div className="field">
+            {/* create onClick save category in button */}
+            <button
+              className="button is-link"
+              label="Submit"
+              onClick={() => {
+                handleUpdateCategory();
+               }}
+            >
+              Submit
+            </button>
+            <button
+              className="button is-warning ml-3"
+              label="Cancel"
+              onClick={() => {
+                navigate(-1);
+               }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
