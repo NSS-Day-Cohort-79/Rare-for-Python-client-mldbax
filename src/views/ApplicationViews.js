be6c32f4663aa5323d5294f06ad3cd617510b078
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-26 11:05:51 -0500

    hooked up EditTag route

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -13,6 +13,7 @@ import { CreatePost } from "../components/posts/CreatePost";
 import { EditPost } from "../components/posts/EditPost";
 import { EditCategory } from "../components/categories/EditCategory";
 import { NewCommentForm } from "../components/comments/NewCommentForm";
+import { EditTag } from "../components/tags/EditTag";
 
 export const ApplicationViews = ({ token, setToken }) => {
   return (
@@ -42,6 +43,9 @@ export const ApplicationViews = ({ token, setToken }) => {
           <Route path="/tags">
             <Route index element={<TagList />} />
             <Route path="new" element={<CreateTag />} />
+          <Route path=":id">
+            <Route path="edit" element={<EditTag />} />
+          </Route>
           </Route>
         </Route>
       </Routes>
